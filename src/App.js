1af6fb2cd9bea88b39693e533e4cf1aec70ed2bb
Caleb Nash <calebnash@Calebs-MacBook-Pro.local>
2020-09-28 14:35:28 -0400

    complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      numLike: 0,
+      plural: 'Likes'
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    let count = this.state.numLike;
+    console.log(count);
+    count += 1;
+    this.setState({numLike: count});
+
+    if(count === 1){
+      this.setState({plural: 'Like'});
+    }else{
+      this.setState({plural: 'Likes'});
+    }
+ }
+
+
+  render() {
+    return (
+      <div className='container'>
+      <button className='likes' onClick = {this.handleClick}> {this.state.numLike} {this.state.plural} < /button>
+      </div>
+    )
+  }
 }
 
 export default App;
